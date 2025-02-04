df917cb228e73d69f0c289d7adb12160176dde6f
Mariana Meyer <92270090+marianameyer@users.noreply.github.com>
2025-02-03 21:29:33 -0300

    Update target.py

--- target.py
+++ target.py
@@ -1,10 +1,15 @@
 # Target a number
 
+""" Returns the first and last indexes of the number in an array. If the number isn't there, 
+returns [-1, -1]."""
+
+# Defining the array and the targets
 arr = [2,4,5,5,5,5,5,7,9,9]
 target1 = 5
 target2 = 3
 target3 = 9
 
+# Function
 def first_and_last(arr, target):
     for i in range(len(arr)):
         if arr[i] == target:
