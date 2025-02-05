9015f6f994aa958a3df60d27397e0fb520703a15
Mariana Meyer <92270090+marianameyer@users.noreply.github.com>
2025-02-04 22:34:46 -0300

    Add files via upload

---
+++ list_comprehension.py
@@ -0,0 +1,17 @@
+# List Comprehension
+
+arr = [i for i in range(5)]
+print(arr)
+print('\n')
+
+# A different result
+arr2 = [i+i for i in range(5)]
+#print(arr2)
+#print('\n')
+
+# 2-D Lists
+
+# Array with size = 4 and all zeros
+#arr2 = [[0]*4 for i in range(4)]
+#print(arr2)
+#print('\n')
